4f1ef615a710dafe35be6eff5478e1e0305f469b
VIEvgeniy <vakevg@yandex.ru>
2024-03-02 23:45:46 +0300

    modified:   src/index.html 	modified:   src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,32 @@ export default function demo(value) {
   return `Demo: ${value}`;
 }
 
-console.log("app.js included");
+console.log("start");
+
+const img = document.documentElement.children[1].children[0];
+
+img.style.cssText = "position:absolute;";
+let cellWidth = document.documentElement.clientWidth >> 2;
+let cellHeight = document.documentElement.clientHeight >> 2;
+let posX = Math.round(3 * Math.random());
+let posY = Math.round(3 * Math.random());
+let coordX = cellWidth * posX;
+let coordY = cellHeight * posY;
+
+setInterval(() => {
+  cellWidth = document.documentElement.clientWidth >> 2;
+  cellHeight = document.documentElement.clientHeight >> 2;
+  let newPosX = Math.round(3 * Math.random());
+  let newPosY = Math.round(3 * Math.random());
+  while (posX == newPosX && posY == newPosY) {
+    if (posX == newPosX) newPosX = Math.round(3 * Math.random());
+    if (posY == newPosY) newPosY = Math.round(3 * Math.random());
+  }
+  posX = newPosX;
+  posY = newPosY;
+  coordX = cellWidth * posX;
+  coordY = cellHeight * posY;
+  img.style.left = coordX + "px";
+  img.style.top = coordY + "px";
+  console.log("pos: ", posX, ", ", posY);
+}, 1500);
