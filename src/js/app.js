46bd47cf1a12342da8166e62743fd06f712ced6a
VIEvgeniy <vakevg@yandex.ru>
2024-03-05 02:08:47 +0300

    modified:   src/css/style.css 	modified:   src/index.html 	modified:   src/js/app.js 	deleted:    src/js/app.test.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,35 @@
 // TODO: write code here
 
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
-
 console.log("start");
 
-const img = document.documentElement.children[1].children[0];
+// const img = document.documentElement.children[1].children[0];
 
-img.style.cssText = "position:absolute;";
-let cellWidth = document.documentElement.clientWidth >> 2;
-let cellHeight = document.documentElement.clientHeight >> 2;
+// img.style.cssText = "position:absolute;";
+// let cellWidth = document.documentElement.clientWidth >> 2;
+// let cellHeight = document.documentElement.clientHeight >> 2;
 let posX = Math.round(3 * Math.random());
 let posY = Math.round(3 * Math.random());
-let coordX = cellWidth * posX;
-let coordY = cellHeight * posY;
+// let coordX = cellWidth * posX;
+// let coordY = cellHeight * posY;
+const table = document.documentElement.children[1].children[0];
+table.children[posY].children[posX].children[0].style.visibility = "visible";
 
 setInterval(() => {
-  cellWidth = document.documentElement.clientWidth >> 2;
-  cellHeight = document.documentElement.clientHeight >> 2;
+  // cellWidth = document.documentElement.clientWidth >> 2;
+  // cellHeight = document.documentElement.clientHeight >> 2;
   let newPosX = Math.round(3 * Math.random());
   let newPosY = Math.round(3 * Math.random());
   while (posX == newPosX && posY == newPosY) {
     if (posX == newPosX) newPosX = Math.round(3 * Math.random());
     if (posY == newPosY) newPosY = Math.round(3 * Math.random());
   }
+  table.children[posY].children[posX].children[0].style.visibility = "hidden";
+  table.children[newPosY].children[newPosX].children[0].style.visibility = "visible";
   posX = newPosX;
   posY = newPosY;
-  coordX = cellWidth * posX;
-  coordY = cellHeight * posY;
-  img.style.left = coordX + "px";
-  img.style.top = coordY + "px";
-  console.log("pos: ", posX, ", ", posY);
-}, 1500);
+  // coordX = cellWidth * posX;
+  // coordY = cellHeight * posY;
+  // img.style.left = coordX + "px";
+  // img.style.top = coordY + "px";
+  // console.log("pos: ", posX, ", ", posY);
+}, 500);
